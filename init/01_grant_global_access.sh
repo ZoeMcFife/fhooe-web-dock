6da82ce4acb4443202a2f3d3a8d036eb7186e054
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2026-03-10 22:55:04 +0100

    Move database initialization to image
    Fixes an issue on macOS that the init script cannot be run if executable permissions are not set.

--- init/01_grant_global_access.sh
+++ init/01_grant_global_access.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-mariadb -u root -p"$MYSQL_ROOT_PASSWORD" <<-EOSQL
+# Root can connect without password via Unix socket during init (MYSQL_ROOT_PASSWORD is not exported when using MYSQL_RANDOM_ROOT_PASSWORD)
+mariadb -u root <<-EOSQL
     GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';
     FLUSH PRIVILEGES;
 EOSQL
